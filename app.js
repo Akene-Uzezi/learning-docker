357c035990317db73c590da2ebc3afa63e0145ca
Akene Uzezi <xtremejoexj@gmail.com>
2026-04-23 11:32:07 +0100

    wrote some basic api routes

--- app.js
+++ app.js
@@ -5,23 +5,40 @@ const port = 1337;
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.get("/setup", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
+    const data = await pool.query("select * from schools");
+    res.status(200).send(data.rows);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
   }
 });
 
-app.post("/", (req, res) => {
+app.get("/setup", async (req, res) => {
+  try {
+    await pool.query(
+      "create table schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address varchar(100))",
+    );
+    res.status(200).send({ message: "successfully created table" });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/", async (req, res) => {
   const { name, location } = req.body;
-  res.status(200).send({
-    message: `your keys were ${name}, ${location}`,
-  });
+  try {
+    await pool.query("insert into schools (name, address) values ($1, $2)", [
+      name,
+      location,
+    ]);
+    res.status(200).send({ message: "Successfully added child" });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(port, () => {
